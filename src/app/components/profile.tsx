55f57d2e40fb1a58491910df2dd702a4b558a4d1
unknown <natan.br15@gmail.com>
2025-03-14 10:13:25 -0300

    :sparkles: header

--- src/app/components/profile.tsx
+++ src/app/components/profile.tsx
@@ -1,18 +1,25 @@
 import Image from "next/image";
 import userIcon from "../../../public/images/user-svgrepo-com.svg";
+import Link from "next/link";
+
+interface ProfileProps {
+
+}
 
 
-const Profile = () => {
+const Profile = ({  }: ProfileProps) => {
     return (
-        <div className="flex flex-col items-center">
-            <Image 
-                src={userIcon}
-                alt="User Icon"
-                height={32}
-                width={32}
-            />
-            <h2>Login</h2>
-        </div>
+        <Link href="/">
+            <div className="flex flex-col items-center">
+                <Image 
+                    src={userIcon}
+                    alt="User Icon"
+                    height={32}
+                    width={32}
+                />
+                <h2>My Profile</h2>
+            </div>
+        </Link>
     )
 }
 
